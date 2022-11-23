0f48e5290b6c31e36732b3dce57153fdccc18cf9
robonexx <robertwagar@gmail.com>
2022-11-23 10:14:41 +0100

    functions repo

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,9 @@
 // Don't touch this array
-let names = ["Alex", "Sam", "John", "Lucia", "Marco", "Kristina"];
+let names = ['Alex', 'Sam', 'John', 'Lucia', 'Marco', 'Kristina'];
 
 // Your code here
+
+function exercise1() {
+  console.log('Hi! My name is Alex');
+}
+exercise1();
