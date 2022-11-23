75cd8a775b267709270925d9bdf1adf4da53e691
robonexx <robertwagar@gmail.com>
2022-11-23 10:20:50 +0100

    exercise 2

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,17 @@ let names = ['Alex', 'Sam', 'John', 'Lucia', 'Marco', 'Kristina'];
 
 // Your code here
 
+// exercise 1
 function exercise1() {
   console.log('Hi! My name is Alex');
 }
 exercise1();
+
+// exercise 2
+
+let name = names[Math.floor(Math.random() * names.length)];
+function exercise2(input) {
+  return `Hi! My name is ${input}`;
+}
+
+console.log(exercise2(name));
