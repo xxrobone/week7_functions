789cc35e80d8b659ce0cd14b611a9cc3f3eb1e5a
robonexx <robertwagar@gmail.com>
2022-11-23 10:32:21 +0100

    exercises 2,3,4,5

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 // Don't touch this array
 let names = ['Alex', 'Sam', 'John', 'Lucia', 'Marco', 'Kristina'];
+let rndName = names[Math.floor(Math.random() * names.length)];
 
 // Your code here
 
@@ -9,11 +10,11 @@ function exercise1() {
 }
 exercise1();
 
-// exercise 2
-
-let name = names[Math.floor(Math.random() * names.length)];
-function exercise2(input) {
-  return `Hi! My name is ${input}`;
+// exercise 2 // exercise 3 (add an argument/ value) // exercise 4 (add lastname) // exercise 5 (add name + sson as parameter/ value for lastname)
+function exercise2(name = 'Alex', lastname = name + 'sson') {
+  return `Hi! My name is ${name} ${lastname}`;
 }
 
-console.log(exercise2(name));
+console.log(exercise2('Milo'));
+
+// exercise 4
