4fb91c39b41772e64e912ef02fd7d32fe3bc0e06
robonexx <robertwagar@gmail.com>
2022-11-23 10:59:30 +0100

    exercise 7 & 8

--- js/functions.js
+++ js/functions.js
@@ -10,7 +10,8 @@ function exercise1() {
 }
 exercise1();
 
-// exercise 2 // exercise 3 (add an argument/ value) // exercise 4 (add lastname) // exercise 5 (add name + sson as parameter/ value for lastname)
+// exercise 2 // exercise 3 (add an argument/ value) // exercise 4 (add lastname)
+// exercise 5 (add name + sson as parameter/ value for lastname)
 function exercise2(name = 'Alex', lastname = name + 'sson') {
   return `Hi! My name is ${name} ${lastname}`;
 }
@@ -18,7 +19,7 @@ function exercise2(name = 'Alex', lastname = name + 'sson') {
 console.log(exercise2('Milo'));
 
 // exercise 6 // exercise 7
-function exercise6(input) {
+function exercise6(input, input2) {
   let div = document.querySelector('#exercise6');
   let para = document.createElement('p');
   para.style.color = 'black';
@@ -27,7 +28,19 @@ function exercise6(input) {
   para.style.textAlign = 'center';
   para.innerText = input;
   div.appendChild(para);
+
+  let para2 = document.createElement('p');
+  para2.style.color = '#fafafa';
+  para2.style.backgroundColor = '#282828';
+  para2.style.borderRadius = '5px';
+  para2.style.textAlign = 'center';
+  para2.innerText = input;
+  para2.textContent = input2;
+
+  div.append(para2);
 }
-exercise6('Here I AM!');
+exercise6('Here I AM!', exercise2('Milo', 'Waegar'));
+exercise6('', exercise2('Milo'));
+exercise6(exercise2());
 
 // exercise 7
